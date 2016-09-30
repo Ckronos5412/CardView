a210fc4ac65947d5106abb0aff979cd560476e73
Ckronos5412 <crish.g.e@gmail.com>
2016-09-30 17:59:09 -0400

    fade_it y backtoolbar

--- app/src/main/java/demo/cardviewphpmysql/Detalle.java
+++ app/src/main/java/demo/cardviewphpmysql/Detalle.java
@@ -1,24 +1,48 @@
 package demo.cardviewphpmysql;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Detalle extends AppCompatActivity {
 
     private TextView textview;
+    private ImageView img;
+    private Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle);
 
+        agregarToolbar();
+
         Intent intent = getIntent();
         String Datos = intent.getExtras().getString("descrip");
 
         textview = (TextView)findViewById(R.id.textView);
+        img = (ImageView)findViewById(R.id.imageView);
 
+        anim = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+
+
+        img.setAnimation(anim);
         textview.setText(Datos);
+    }
+
+    private void agregarToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setDisplayShowHomeEnabled(true);
+        }
     }
 }
--- app/src/main/java/demo/cardviewphpmysql/MainActivity.java
+++ app/src/main/java/demo/cardviewphpmysql/MainActivity.java
@@ -1,10 +1,12 @@
 package demo.cardviewphpmysql;
 
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import org.json.JSONArray;
